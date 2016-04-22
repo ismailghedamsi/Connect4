4d0d53c9b8f4fe9b594b274d9729bee2a6c5b256
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2016-04-22 17:49:45 -0400

    Add exe folder contaning Connect4.java Connect4.class

---
+++ exe/Connect4.java
@@ -0,0 +1,9 @@
+package exe;
+
+import gui.*;
+public class Connect4{
+  public static void main(String[] args){
+    C4Gui gui = new C4Gui(); 
+    C4Controller controller = new C4Controller(gui);
+ }
+}
